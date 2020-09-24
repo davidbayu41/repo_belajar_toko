107043706c9e64274341493b53db5a6cc50c1532
davidbayu41 <daprot95@gmail.com>
2020-09-24 21:06:06 +0700

    Menambah autensikasi API dengan JWT

--- routes/api.php
+++ routes/api.php
@@ -12,16 +12,17 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register', 'UserController@register');
+Route::post('/login', 'UserController@login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['jwt.verify']], function ()
+{
+	Route::get('/kelas', 'KelasController@show');
+	Route::post('/kelas', 'KelasController@store');
+
+	Route::get('/siswa', 'KelasController@show');
+	Route::get('/siswa/{id}', 'KelasController@detail');
+	Route::post('/siswa', 'SiswaController@store');
+	Route::put('/siswa/{id}', 'SiswaController@update');
+	Route::delete('/siswa/{id}', 'SiswaController@destroy');
 });
-
-Route::get('/kelas', 'KelasController@show');
-Route::post('/kelas', 'KelasController@store');
-
-Route::get('/siswa', 'KelasController@show');
-Route::get('/siswa/{id}', 'KelasController@detail');
-Route::post('/siswa', 'SiswaController@store');
-Route::put('/siswa/{id}', 'SiswaController@update');
-Route::delete('/siswa/{id}', 'SiswaController@destroy');
